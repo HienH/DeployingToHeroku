8fb8533797c11e8bbfeb639f59075a3c1f78f63e
hien hang <hienhang@hotmail.co.uk>
2020-10-03 19:03:21 +0100

    update package.json

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static('./dist/heroku-deployment'));
+
+app.use(express.static(__dirname + '/dist/heroku-deployment'));
 app.get('/*', function (req, res) {
-    res.sendFile(__dirname + '/dist/heroku-deployment/index.html');
+    res.sendFile(path.join(__dirname +
+        '/dist/heroku-deployment/index.html'));
 });
 app.listen(process.env.PORT || 8080);
